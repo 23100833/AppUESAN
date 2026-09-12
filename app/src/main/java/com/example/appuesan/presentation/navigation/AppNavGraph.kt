ee8f1c33a9f560c0902a2dfbf0a22555cbb5654e
Kiara Ruiz <laboratorioesan@esan.edu.pe>
2026-09-12 17:26:38 -0500

    Register Login and home Screen

---
+++ app/src/main/java/com/example/appuesan/presentation/navigation/AppNavGraph.kt
@@ -0,0 +1,23 @@
+package com.example.appuesan.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHost
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.appuesan.presentation.auth.LoginScreen
+import com.example.appuesan.presentation.auth.RegisterScreen
+import com.example.appuesan.presentation.home.HomeScreen
+
+@Composable
+fun AppNavGraph(){
+    val navController = rememberNavController()
+
+    NavHost(navController = navController,
+        startDestination = "login")
+    {
+        composable("register"){ RegisterScreen(navController) }
+        composable("login"){ LoginScreen(navController) }
+        composable("home"){ HomeScreen() }
+    }
+}
